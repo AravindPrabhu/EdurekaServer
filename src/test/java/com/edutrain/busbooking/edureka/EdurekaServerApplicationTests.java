943ca8d29b4746b84bc35e3e8a6938815ddc08af
AravindPrabhu <gg.aravind@yahoo.com>
2023-09-24 18:30:32 +0530

    Initial commit

---
+++ src/test/java/com/edutrain/busbooking/edureka/EdurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edutrain.busbooking.edureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
